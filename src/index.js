37590dc2610149eea0a3a94770b20feaf7b9010b
Austin McShane <amcshane@Austins-Laptop.local>
2023-12-22 11:51:31 -0800

    Added middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './containers/App'
 import { searchRobots } from './reducers';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const container = document.getElementById('root');
 const root = createRoot(container);
