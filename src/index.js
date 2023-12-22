ec2ebaf3606344708803e8fdcd0771fae44b0a2b
Austin McShane <amcshane@Austins-Laptop.local>
2023-12-22 13:37:24 -0800

    Added redux async actions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import { thunk as thunkMiddleware } from 'redux-thunk';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './containers/App'
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = 
+createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 const container = document.getElementById('root');
 const root = createRoot(container);
